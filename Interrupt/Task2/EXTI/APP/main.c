b84ad489bd7450a719c5e08299237de88703de1c
Hosam Ayoub <hosamayoub50@gmail.com>
2021-10-23 19:09:53 +0200

    Final Commit

--- Interrupt/Task2/EXTI/APP/main.c
+++ Interrupt/Task2/EXTI/APP/main.c
@@ -6,14 +6,15 @@
 #include "main.h"
 #define S	0b10010001
 
-void Toggle1 (void);
-void Toggle2 (void);
-void Toggle3 (void);
+void ToggleLED1 (void);
+void ToggleLED2 (void);
+void ToggleLED3 (void);
 
 void main (void)
 {
 	DIO_u8SetPortDirection(DIO_PORTA, S);
 	LCD_voidInitialize();
+	LCD_voidSendCommand(0x01);
 	LCD_voidGoToXY(LCD_ROW0, LCD_COLUMN0);
 	LCD_voidSendString("LED1");
 	LCD_voidGoToXY(LCD_ROW0, LCD_COLUMN6);
@@ -32,9 +33,9 @@ void main (void)
 	DIO_u8SetPinValue(DIO_PORTD, DIO_PIN3, DIO_PULLUP);
 	DIO_u8SetPinDirection(DIO_PORTB, DIO_PIN2, DIO_PIN_INPUT);
 	DIO_u8SetPinValue(DIO_PORTB, DIO_PIN2, DIO_PULLUP);
-	EXTI_voidInt0SetCallBack(&Toggle1);
-	EXTI_voidInt1SetCallBack(&Toggle2);
-	EXTI_voidInt2SetCallBack(&Toggle3);
+	EXTI_voidInt0SetCallBack(&ToggleLED1);
+	EXTI_voidInt1SetCallBack(&ToggleLED2);
+	EXTI_voidInt2SetCallBack(&ToggleLED3);
 	GIE_voidEnableGlobalInterrupt();
 	EXTI_voidInit();
 	while (1)
@@ -43,9 +44,9 @@ void main (void)
 	}
 }
 
-void Toggle1 (void)
+void ToggleLED1 (void)
 {
-	/*Toggle */
+	/*Toggle LED*/
 	u8 Local_u8PinState;
 	DIO_u8TogglePin(DIO_PORTA, DIO_PIN0);
 	DIO_u8GetPinValue(DIO_PORTA, DIO_PIN0, &Local_u8PinState);
@@ -65,9 +66,9 @@ void Toggle1 (void)
 	}
 }
 
-void Toggle2 (void)
+void ToggleLED2 (void)
 {
-	/*Toggle */
+	/*Toggle LED*/
 	u8 Local_u8PinState;
 	DIO_u8TogglePin(DIO_PORTA, DIO_PIN4);
 	DIO_u8GetPinValue(DIO_PORTA, DIO_PIN4, &Local_u8PinState);
@@ -87,9 +88,9 @@ void Toggle2 (void)
 	}
 }
 
-void Toggle3 (void)
+void ToggleLED3 (void)
 {
-	/*Toggle */
+	/*Toggle LED*/
 	u8 Local_u8PinState;
 	DIO_u8TogglePin(DIO_PORTA, DIO_PIN7);
 	DIO_u8GetPinValue(DIO_PORTA, DIO_PIN7, &Local_u8PinState);
