46b3c42c2857aea86ef96e167eb195354c27f1e2
Hosam Ayoub <hosamayoub50@gmail.com>
2021-10-23 13:41:40 +0200

    Final Commit

--- Interrupt/Task2/EXTI/APP/main.c
+++ Interrupt/Task2/EXTI/APP/main.c
@@ -4,22 +4,22 @@
 /********************************************************************************************/
 /********************************************************************************************/
 #include "main.h"
-#define LEDS	0b10010001
+#define S	0b10010001
 
-void ToggleLed1 (void);
-void ToggleLed2 (void);
-void ToggleLed3 (void);
+void Toggle1 (void);
+void Toggle2 (void);
+void Toggle3 (void);
 
 void main (void)
 {
-	DIO_u8SetPortDirection(DIO_PORTA, LEDS);
+	DIO_u8SetPortDirection(DIO_PORTA, S);
 	LCD_voidInitialize();
 	LCD_voidGoToXY(LCD_ROW0, LCD_COLUMN0);
-	LCD_voidSendString("LCD1");
+	LCD_voidSendString("LED1");
 	LCD_voidGoToXY(LCD_ROW0, LCD_COLUMN6);
-	LCD_voidSendString("LCD2");
+	LCD_voidSendString("LED2");
 	LCD_voidGoToXY(LCD_ROW0, LCD_COLUMN12);
-	LCD_voidSendString("LCD3");
+	LCD_voidSendString("LED3");
 	LCD_voidGoToXY(LCD_ROW1, LCD_COLUMN1);
 	LCD_voidSendString("OFF");
 	LCD_voidGoToXY(LCD_ROW1, LCD_COLUMN7);
@@ -32,9 +32,9 @@ void main (void)
 	DIO_u8SetPinValue(DIO_PORTD, DIO_PIN3, DIO_PULLUP);
 	DIO_u8SetPinDirection(DIO_PORTB, DIO_PIN2, DIO_PIN_INPUT);
 	DIO_u8SetPinValue(DIO_PORTB, DIO_PIN2, DIO_PULLUP);
-	EXTI_voidInt0SetCallBack(&ToggleLed1);
-	EXTI_voidInt1SetCallBack(&ToggleLed2);
-	EXTI_voidInt2SetCallBack(&ToggleLed3);
+	EXTI_voidInt0SetCallBack(&Toggle1);
+	EXTI_voidInt1SetCallBack(&Toggle2);
+	EXTI_voidInt2SetCallBack(&Toggle3);
 	GIE_voidEnableGlobalInterrupt();
 	EXTI_voidInit();
 	while (1)
@@ -43,9 +43,9 @@ void main (void)
 	}
 }
 
-void ToggleLed1 (void)
+void Toggle1 (void)
 {
-	/*Toggle LED*/
+	/*Toggle */
 	u8 Local_u8PinState;
 	DIO_u8TogglePin(DIO_PORTA, DIO_PIN0);
 	DIO_u8GetPinValue(DIO_PORTA, DIO_PIN0, &Local_u8PinState);
@@ -65,9 +65,9 @@ void ToggleLed1 (void)
 	}
 }
 
-void ToggleLed2 (void)
+void Toggle2 (void)
 {
-	/*Toggle LED*/
+	/*Toggle */
 	u8 Local_u8PinState;
 	DIO_u8TogglePin(DIO_PORTA, DIO_PIN4);
 	DIO_u8GetPinValue(DIO_PORTA, DIO_PIN4, &Local_u8PinState);
@@ -87,9 +87,9 @@ void ToggleLed2 (void)
 	}
 }
 
-void ToggleLed3 (void)
+void Toggle3 (void)
 {
-	/*Toggle LED*/
+	/*Toggle */
 	u8 Local_u8PinState;
 	DIO_u8TogglePin(DIO_PORTA, DIO_PIN7);
 	DIO_u8GetPinValue(DIO_PORTA, DIO_PIN7, &Local_u8PinState);
